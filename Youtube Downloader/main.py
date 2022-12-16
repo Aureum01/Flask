bad1ecbf8000a81332e4c74129d1388b7a86cdc5
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2022-12-15 23:20:03 -0500

    Create main.py

---
+++ Youtube Downloader/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from pytube import YouTube
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download', methods=['POST'])
+def download():
+    # Get the URL of the video from the form submission
+    url = request.form['url']
+
+    # Create a YouTube instance for the video
+    video = YouTube(url)
+
+    # Get the highest resolution video stream
+    video_stream = video.streams.get_highest_resolution()
+
+    # Get the first available audio stream
+    audio_stream = video.streams.filter(only_audio=True).first()
+
+    # Download the video and audio streams
+    video_stream.download(output_path='C:/Users/User/Downloads')
+    audio_stream.download(output_path='C:/Users/User/Downloads')
+
+    return 'Thanks for using my program!'
+
+if __name__ == '__main__':
+    app.run()
